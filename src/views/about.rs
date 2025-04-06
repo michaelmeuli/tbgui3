41b2e99563de83061d51566366d9633cdaaa294d
Michael Meuli <mmeuli@imm.uzh.ch>
2025-04-06 22:05:25 +0200

    add spacing to logo

--- src/views/about.rs
+++ src/views/about.rs
@@ -1,6 +1,7 @@
 use crate::app::{App, Message};
 use crate::fl;
 use cosmic::iced::Alignment;
+use cosmic::iced_runtime::task::widget;
 use cosmic::widget;
 use cosmic::Element;
 use cosmic::{cosmic_theme, theme};
@@ -13,7 +14,7 @@ where
     Self: cosmic::Application,
 {
     pub fn about(&self) -> Element<Message> {
-        let cosmic_theme::Spacing { space_xxs, .. } = theme::active().cosmic().spacing;
+        let cosmic_theme::Spacing { space_xxs, space_l, .. } = theme::active().cosmic().spacing;
         let icon = widget::svg(widget::svg::Handle::from_memory(APP_ICON));
         let title = widget::text::title3(fl!("app-title"));
         let hash = env!("VERGEN_GIT_SHA");
@@ -23,7 +24,9 @@ where
             .on_press(Message::OpenRepositoryUrl)
             .padding(0);
         widget::column()
+            .push(widget::vertical_space().height(space_l))
             .push(icon)
+            .push(widget::vertical_space().height(space_l))
             .push(title)
             .push(link)
             .push(
