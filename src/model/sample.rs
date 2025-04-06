93a9d2bd22ab663f2f0330f895ec8c76d698026c
Michael Meuli <mmeuli@imm.uzh.ch>
2025-04-06 21:50:16 +0200

    add sample.rs to model folder

---
+++ src/model/sample.rs
@@ -0,0 +1,60 @@
+use cosmic::widget::{checkbox, text};
+use cosmic::Element;
+use cosmic::iced::Length;
+use uuid::Uuid;
+
+
+
+
+#[derive(Debug, Clone)]
+pub struct Item {
+    pub id: Uuid,
+    pub sample: String,
+    pub is_checked: bool,
+}
+
+#[derive(Debug, Clone)]
+pub enum ItemMessage {
+    CheckboxToggled(bool),
+}
+
+impl Item {
+    pub fn update(&mut self, message: ItemMessage) {
+        match message {
+            ItemMessage::CheckboxToggled(is_checked) => {
+                self.is_checked = is_checked;
+            }
+        }
+    }
+
+    pub fn view(&self) -> Element<ItemMessage> {
+        checkbox(&self.sample, self.is_checked)
+            .on_toggle(ItemMessage::CheckboxToggled)
+            .width(Length::Fill)
+            .size(17)
+            .into()
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
+pub enum Filter {
+    #[default]
+    All,
+    Unchecked,
+    Checked,
+}
+
+impl Filter {
+    pub fn matches(self, item: &Item) -> bool {
+        match self {
+            Filter::All => true,
+            Filter::Unchecked => !item.is_checked,
+            Filter::Checked => item.is_checked,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct RemoteState {
+    pub items: Vec<Item>,
+}
