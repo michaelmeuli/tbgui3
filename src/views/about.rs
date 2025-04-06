deea0e0e947edee9640d68b5ec44ad48d8b10a9e
Michael Meuli <mmeuli@imm.uzh.ch>
2025-04-06 11:50:36 +0200

    update

--- src/views/about.rs
+++ src/views/about.rs
@@ -14,19 +14,14 @@ where
 {
     pub fn about(&self) -> Element<Message> {
         let cosmic_theme::Spacing { space_xxs, .. } = theme::active().cosmic().spacing;
-
         let icon = widget::svg(widget::svg::Handle::from_memory(APP_ICON));
-
         let title = widget::text::title3(fl!("app-title"));
-
         let hash = env!("VERGEN_GIT_SHA");
         let short_hash: String = hash.chars().take(7).collect();
         let date = env!("VERGEN_GIT_COMMIT_DATE");
-
         let link = widget::button::link(REPOSITORY)
             .on_press(Message::OpenRepositoryUrl)
             .padding(0);
-
         widget::column()
             .push(icon)
             .push(title)
